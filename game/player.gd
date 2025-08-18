09e0d0826d5d387c12aab3c4b4f3f6c6bf8c9823
Quixotiz <korsholmlasse@gmail.com>
2025-08-18 08:29:47 +0200

    playervirke?

--- game/player.gd
+++ game/player.gd
@@ -1,25 +1,23 @@
-extends CharacterBody2D
+extends Area2D
 
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	screen_size = get_viewport_rect().size
 
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		position += velocity * delta
+		position = position.clamp(Vector2.ZERO, screen_size)
