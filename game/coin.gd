372a0cbddd4a865176a2970e4ca1139b14df8c85
SigurdFraRG <sorenlarsen2006@gmail.com>
2025-08-26 13:03:01 +0200

    hehehe

--- game/coin.gd
+++ game/coin.gd
@@ -1,6 +1,6 @@
 extends Area2D
 # Reference to the collectible scene itself (so we can respawn it)
-@export var collectible_scene: PackedScene
+@export var coin_scene: PackedScene
 # Area where collectibles can spawn
 @export var spawn_area: Rect2 = Rect2(Vector2.ZERO, Vector2(1000, 600))
 
@@ -9,11 +9,11 @@ func _ready():
 
 func _on_body_entered(body):
 		queue_free() # remove current collectible
-		_spawn_new_collectible()
+		_spawn_new_coin()
 
-func _spawn_new_collectible():
-	if collectible_scene:
-		var new_one = collectible_scene.instantiate()
+func _spawn_new_coin():
+	if coin_scene:
+		var new_one = coin_scene.instantiate()
 		var spawn_pos = Vector2(
 			randi_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x),
 			randi_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
