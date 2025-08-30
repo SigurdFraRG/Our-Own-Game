66daf0f9aefccd9e7a1d3ecb90721e13a7bee214
SigurdFraRG <sorenlarsen2006@gmail.com>
2025-08-30 12:22:27 +0200

    done

--- game/coin.gd
+++ game/coin.gd
@@ -1,22 +1,10 @@
 extends Area2D
-# Reference to the collectible scene itself (so we can respawn it)
-@export var coin_scene: PackedScene
-# Area where collectibles can spawn
-@export var spawn_area: Rect2 = Rect2(Vector2.ZERO, Vector2(1000, 600))
+
+signal collected
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
-		queue_free() # remove current collectible
-		_spawn_new_coin()
-
-func _spawn_new_coin():
-	if coin_scene:
-		var new_one = coin_scene.instantiate()
-		var spawn_pos = Vector2(
-			randi_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x),
-			randi_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
-		)
-		new_one.position = spawn_pos
-		get_tree().current_scene.add_child(new_one)
+	emit_signal("collected") # notify the spawner
+	queue_free() # remove this coin
---
+++ game/main.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var coin_scene: PackedScene
+@export var spawn_area: Rect2 = Rect2(Vector2(50,50), Vector2(1000, 550))
+@onready var counter_label: Label = $CanvasLayer/Label
+var coins_collected = 0
+
+func _ready():
+	_spawn_new_coin()
+
+func _spawn_new_coin():
+	if coin_scene:
+		var new_coin = coin_scene.instantiate()
+		var spawn_pos = Vector2(
+			randi_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x),
+			randi_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
+		)
+		new_coin.position = spawn_pos
+		
+		new_coin.connect("collected", Callable(self, "_on_coin_collected"))
+
+		add_child(new_coin)
+	else:
+		push_error("coin_scene not set in Inspector!")
+
+func _on_coin_collected():
+	coins_collected += 1
+	counter_label.text = "Coins Collected: %d" % coins_collected
+	$"coinsound".play()
+	_spawn_new_coin()
